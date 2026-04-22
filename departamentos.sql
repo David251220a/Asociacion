bad0cb8d336053f4ac1e454b54de4c3e38d32300
David PC Caja <82409787+David251220a@users.noreply.github.com>
2026-04-22 08:52:39 -0400

    importes
    importes necesario para distritos y otros

---
+++ departamentos.sql
@@ -0,0 +1,24 @@
+﻿/* Data for the 'Query_Result' table  (Records 1 - 18) */
+
+INSERT INTO [Query_Result]
+  ([id], [descripcion])
+VALUES 
+  (10, N'PARAGUARI'),
+  (9, N'MISIONES'),
+  (7, N'CAAZAPA'),
+  (4, N'CORDILLERA'),
+  (5, N'GUAIRA'),
+  (18, N'CANINDEYU'),
+  (1, N'CAPITAL'),
+  (15, N'PTE. HAYES'),
+  (16, N'BOQUERON'),
+  (12, N'CENTRAL'),
+  (14, N'AMAMBAY'),
+  (2, N'CONCEPCION'),
+  (6, N'CAAGUAZU'),
+  (11, N'ALTO PARANA'),
+  (17, N'ALTO PARAGUAY'),
+  (13, N'NEEMBUCU'),
+  (3, N'SAN PEDRO'),
+  (8, N'ITAPUA')
+GO
---
+++ departamentos_muni.sql
@@ -0,0 +1,25 @@
+﻿/* Data for the 'muni_departamentos' table  (Records 1 - 20) */
+
+INSERT INTO `muni_departamentos`
+  (`id`, `departamento_id`, `descripcion`, `estado_id`)
+VALUES 
+  (1, 100, 'SIN ESPECIFICAR', 1),
+  (2, 0, 'CAPITAL', 1),
+  (3, 1, 'CONCEPCION', 1),
+  (4, 2, 'SAN PEDRO', 1),
+  (5, 3, 'CORDILLERA', 1),
+  (6, 4, 'GUAIRA', 1),
+  (7, 5, 'CAAGUAZU', 1),
+  (8, 6, 'CAAZAPA', 1),
+  (9, 7, 'ITAPUA', 1),
+  (10, 8, 'MISIONES', 1),
+  (11, 9, 'PARAGUARI', 1),
+  (12, 10, 'ALTO PARANA', 1),
+  (13, 11, 'CENTRAL', 1),
+  (14, 12, 'NEEMBUCU', 1),
+  (15, 13, 'AMAMBAY', 1),
+  (16, 14, 'CANINDEYU', 1),
+  (17, 15, 'PRESIDENTE HAYES', 1),
+  (18, 16, 'ALTO PARAGUAY', 1),
+  (19, 17, 'BOQUERON', 1),
+  (20, 99, 'OTRAS INSTITUCIONES MUNICIPALES', 1);
